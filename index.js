d3d4b34387da27fb2a6d5dfa80b135cb92990f28
embresse <115603992+embresse@users.noreply.github.com>
2023-04-10 21:21:54 -0400

    built out functions in index.js

--- index.js
+++ index.js
@@ -9,3 +9,127 @@ const Intern = require('./lib/Intern');
 
 const teamArray = [];
 
+const manager = [
+    {
+        type: 'input',
+        message: 'Manager name?',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'Manager email?',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'Manager ID?',
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: 'Office number?',
+        name: 'officenumber'
+    },
+];
+
+function managerPrompt() {
+    inquirer
+        .prompt(manager)
+        .then((answers) => {
+            var manager = new Manager(answers.name, answers.id, answers.email, answers.officenumber);
+            employeeArray.push(manager);
+            addEmployee();
+        });
+};
+
+const engineer  = [
+    {
+        type: 'input',
+        message: 'Engineer name?',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'Engineer email?',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'Engineer ID?',
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: 'Github?',
+        name: 'github'
+    },
+];
+
+function engineerPrompt() {
+    inquirer
+        .prompt(engineer)
+        .then((answers) => {
+            var engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            employeeArray.push(engineer);
+            addEmployee();
+        });
+};
+
+const intern  = [
+    {
+        type: 'input',
+        message: 'Intern name?',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'Intern email?',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'Intern ID?',
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: 'Intern school?',
+        name: 'school'
+    },
+];
+
+function internPrompt() {
+    inquirer
+        .prompt(intern)
+        .then((answers) => {
+            var intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            employeeArray.push(intern);
+            addEmployee();
+        });
+};
+
+function addEmployee(){
+    inquirer
+    .prompt({
+      type: 'list',
+      message: 'Add another employee?',
+      name: 'addEmployee',
+      choices: ['Engineer', 'Intern', 'Finished'],
+    })
+    .then((answer) => {
+      switch (answer.addEmployee) {
+        case 'Engineer':
+          engineerPrompt();
+          break;
+        case 'Intern':
+          internPrompt();
+          break;
+        default:
+          writeToFile(createHTML(employeeArray));
+      }
+    });
+};
+
+function init() {
+    managerPrompt();
+}
